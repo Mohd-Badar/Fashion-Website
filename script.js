de699d8ee1394fb830782c397616601cddfc285f
Mohd Badar <mohd.badar9917@gmail.com>
2026-09-16 13:41:14 +0530

    update

--- script.js
+++ script.js
@@ -174,4 +174,14 @@ document.addEventListener("DOMContentLoaded", () => {
             if (productsSection) productsSection.scrollIntoView({ behavior: "smooth" });
         });
     });
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+if (menuToggle && navMenu) {
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}
 });
+
